6d323c30ec2d0d6e0187b8887fcf00e234abf0d6
Sungwoo.Han <sungwoo.han@9folders.com>
2024-01-05 14:53:52 +0900

    Update MCIMAPAsyncConnection.cpp
    mQueue 가 먼저 memory release 된 현상 방어 코드.

--- src/async/imap/MCIMAPAsyncConnection.cpp
+++ src/async/imap/MCIMAPAsyncConnection.cpp
@@ -100,6 +100,9 @@ IMAPAsyncConnection::IMAPAsyncConnection()
 {
     mSession = new IMAPSession();
     mQueue = new OperationQueue();
+#ifdef DEBUG
+    mQueue->retain();
+#endif
     mDefaultNamespace = NULL;
     mClientIdentity = new IMAPIdentity();
     mLastFolder = NULL;
@@ -127,6 +130,12 @@ IMAPAsyncConnection::~IMAPAsyncConnection()
     MC_SAFE_RELEASE(mLastFolder);
     MC_SAFE_RELEASE(mClientIdentity);
     MC_SAFE_RELEASE(mDefaultNamespace);
+#ifdef DEBUG
+    int queueRetainCount = mQueue->retainCount();
+    if (queueRetainCount > 1) {
+        mQueue->release();
+    }
+#endif
     MC_SAFE_RELEASE(mQueue);
     MC_SAFE_RELEASE(mSession);
 }
